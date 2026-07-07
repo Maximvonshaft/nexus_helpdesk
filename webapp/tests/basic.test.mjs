7762ee03dae5eb07521f0acc3cef06f9e61636b5
moyoyo <temeta@outlook.jp>
2026-07-07 21:37:50 +0200

    unit test update

--- webapp/tests/basic.test.mjs
+++ webapp/tests/basic.test.mjs
@@ -1,6 +1,3 @@
 import test from 'node:test'
-import assert from 'node:assert/strict'
 
-test('webapp test harness is active', () => {
-  assert.equal(typeof test, 'function')
-})
+test('webapp test harness is active', () => {})
