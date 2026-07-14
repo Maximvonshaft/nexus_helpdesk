2c67a550c47753fb7499a06df67a74e8db65b4f8
moyoyo <temeta@outlook.jp>
2026-07-14 12:08:19 +0200

    refactor(frontend): delete duplicate business UI exports

--- webapp/src/shared/ui/business/index.ts
+++
@@ -1,5 +0,0 @@
-export { SafetyGateBadge } from './SafetyGateBadge'
-export type { SafetyGateBadgeProps, SafetyGateState } from './SafetyGateBadge'
-
-export { StatusBadge } from './StatusBadge'
-export type { OperationalStatus, StatusBadgeProps } from './StatusBadge'
